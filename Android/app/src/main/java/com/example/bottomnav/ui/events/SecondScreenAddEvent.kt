7220b2eeff382c070939b94786766de90f5fddde
Freddy <fredericdouamba2014@gmail.com>
2024-05-20 12:06:10 -0700

    SecondScreenAddEvent completed

---
+++ Android/app/src/main/java/com/example/bottomnav/ui/events/SecondScreenAddEvent.kt
@@ -0,0 +1,19 @@
+package com.example.bottomnav.ui.events
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.bottomnav.R
+
+class SecondScreenAddEvent : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.second_screen_add_event, container, false)
+    }
+
+
+}
