9a2370bdde3fb92508c9aa38e4d720e0cc2460f2
Freddy <fredericdouamba2014@gmail.com>
2024-06-05 19:44:11 -0700

    take photo and select photo are working

--- Android/app/src/main/java/com/example/bottomnav/ui/events/SecondScreenAddEvent.kt
+++ Android/app/src/main/java/com/example/bottomnav/ui/events/SecondScreenAddEvent.kt
@@ -1,19 +1,60 @@
 package com.example.bottomnav.ui.events
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.provider.MediaStore
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.example.bottomnav.R
 
-class SecondScreenAddEvent : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.second_screen_add_event, container, false)
+class SecondScreenAddEvent : AppCompatActivity() {
+
+    companion object {
+        const val REQUEST_IMAGE_CAPTURE = 1
+        const val REQUEST_IMAGE_SELECT = 2
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_screen_add_event)
+
+        findViewById<Button>(R.id.buttonTakePhoto).setOnClickListener {
+            dispatchTakePictureIntent()
+        }
+
+        findViewById<Button>(R.id.buttonSelectPhoto).setOnClickListener {
+            dispatchGalleryIntent()
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } else {
+            // Handle the case where no application can handle the intent
+        }
     }
 
 
+    private fun dispatchGalleryIntent() {
+        val pickPhotoIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(pickPhotoIntent, REQUEST_IMAGE_SELECT)
+    }
+
+    // Handle the results of camera or gallery intents
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+                REQUEST_IMAGE_CAPTURE -> {
+                    // Handle the captured image
+                }
+                REQUEST_IMAGE_SELECT -> {
+                    // Handle the selected image from gallery
+                }
+            }
+        }
+    }
 }
